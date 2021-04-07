77f8738d6c6c34eaf2459bd033e975de64785826
7c00 <i@7c00.cc>
2021-04-07 14:05:36 +0800

    Init

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+  id("com.android.application")
+  id("kotlin-android")
+}
+
+android {
+  compileSdk = 30
+  buildToolsVersion = "30.0.3"
+
+  defaultConfig {
+    applicationId = "com.example.app"
+    minSdk = 21
+    targetSdk = 30
+    versionCode = 1
+    versionName = "1.0"
+  }
+  signingConfigs {
+    named("debug") {
+      storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
+      storePassword = "android"
+      keyAlias = "androiddebugkey"
+      keyPassword = "android"
+    }
+  }
+  buildTypes {
+    release {
+      signingConfig = signingConfigs.getByName("debug")
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  kotlinOptions {
+    useIR = true
+  }
+  buildFeatures {
+    compose = true
+  }
+}
+
+dependencies {
+  implementation("androidx.appcompat:appcompat:1.2.0")
+  implementation("com.google.android.material:material:1.3.0")
+  implementation("androidx.compose.ui:ui:1.0.0-beta03")
+  implementation("androidx.compose.material:material:1.0.0-beta03")
+  implementation("androidx.activity:activity-compose:1.3.0-alpha05")
+}
